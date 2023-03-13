1a6cf59e849977dd3940dd84728f89159a6b1d4e
Rishu Rai <54782726+rishurai2001@users.noreply.github.com>
2023-03-13 13:34:16 +0530

    Delete crud springboot directory

--- crud springboot/src/main/java/com/example/crud/repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.crud.models.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Long> {
-
-}
